e754ec351574f1176909ff61ed0cbbfa4bc7f594
SRHRG44 <sergioR8992@gmail.com>
2025-01-16 17:13:19 -0500

    pre lecture 2 commit

---
+++ class_code/week02/functions.js
@@ -0,0 +1,13 @@
+function sayHi(name) {
+    console.log(`Hello ${name}`)
+}
+
+const sayHi2 = (name) => {
+    console.log(`Howdy ${name}`)
+}
+
+const sayHi3 = (name) => {
+    return (`SUPPPPPPPPPP ${name}`)
+}
+
+module.exports = { sayHi, sayHi2, sayHi3 }
---
+++ class_code/week02/useFunctions.js
@@ -0,0 +1,6 @@
+const greetings = require("./functions")
+
+const {sayHi3} = require("./functions")
+
+greetings.sayHi("Sergio")
+sayHi3("Bob")
