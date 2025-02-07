1301ed4801aaa8c9f3387195d2c6b0ae5979486b
SRHRG44 <sergioR8992@gmail.com>
2025-02-06 20:50:16 -0500

    finish commit

--- class_code/week05/client/src/App.jsx
+++ class_code/week05/client/src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   // Add our use states here
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [file, setFile] = useState(null);
 
 
   // promises -> pending -> sucecess or failure
@@ -23,15 +25,16 @@ const App = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const loginForm = new FormData();
-    loginForm.append('username', username);
-    loginForm.append('password', password);
+    const loginForm = {username, password};
+    // loginForm.append('username', username);
+    // loginForm.append('password', password);
+    console.log(loginForm);
 
       try {
-        const response = await fetch(`http://localhost:8000/data`, {
+        const response = await fetch(`http://localhost:8000/login`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: loginForm,
+          body: JSON.stringify(loginForm),
         });
         const data = await response.json();
         console.log(data);
@@ -42,11 +45,29 @@ const App = () => {
       }
   }
 
+  const handleFileUpload = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+      try {
+        const response = await fetch(`http://localhost:8000/upload`, {
+          method: 'POST',
+          body: formData
+        });
+        const data = await response.json();
+        setMessage(data);
+        console.log(data);
+        } catch (error) {
+        console.log(error);
+      }
+  }
+
   return (
     <div>
       <button onClick={handleButton}>Fetch Data</button>
       <p>--------------------------------------------</p>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type= "text"
           placeholder="username"
@@ -59,6 +80,12 @@ const App = () => {
           value={password}
           onChange={(e) => {setPassword(e.target.value)}}
         />
+        <button type="submit">Login</button>
+      </form>
+      <p>-----------------------------------------------</p>
+      <form onSubmit={handleFileUpload}>
+        <input type="file" multiple value={file} onChange={(e) => setFile(e.target.value)} />
+        <button type="submit">Upload File</button>
       </form>
     </div>
   )
--- class_code/week05/server/index.js
+++ class_code/week05/server/index.js
@@ -1,14 +1,28 @@
-
 import express from "express";
 import cors from "cors";
+import multer from "multer";
 // const express = require("express"); // if using common JS (Default)
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        const uniquePrefix = Date.now();
+        cb(null, uniquePrefix + file.fieldname);
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() *1E9)
+        // cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // middlelware
 app.use(cors());
-
+app.use(express.urlencoded({ extended: true })); // this line is used for plain HTML FORMS
+app.use(express.json()); // this line is used for JSON
 
 // routes
 app.get("/", (req, res) => {
@@ -26,6 +40,12 @@ app.get("/data", (req, res) => {
 app.post("/login", (req, res) => {
     console.log(req.body);
     res.json("I got your Information");
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    console.log(req.file);
+    console.log(req.body);
+    res.json("I got your file");
 });
 
 app.listen(PORT, () => {
