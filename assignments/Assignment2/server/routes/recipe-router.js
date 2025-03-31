307600663478eb2305ceb217588bba95c5ebc4d7
Sergio Romero <sergior8992@gmail.com>
2025-03-31 13:40:50 -0400

    initial code completion

---
+++ assignments/Assignment2/server/routes/recipe-router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const recipeController = require('./recipe_controller'); // Import the controller
+
+// GET all recipes
+router.get('/recipe', recipeController.getAllRecipes);
+
+// POST a new recipe
+router.post('/recipe', recipeController.createRecipe);
+
+// GET a recipe by ID
+router.get('/recipe/:id', recipeController.getRecipeById);
+
+// PUT (update) a recipe by ID
+router.put('/recipe/:id', recipeController.updateRecipeById);
+
+// DELETE a recipe by ID
+router.delete('/recipe/:id', recipeController.deleteRecipeById);
+
+module.exports = router;
